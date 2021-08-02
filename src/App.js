815ace99792b62a303ba61951c655e7746a1c6bd
Falsher <ivan.zzaharov@gmail.com>
2021-08-02 21:44:42 +0300

    git added first

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import Counter from './components/coffe/review';
 
 const App = () => (
--- src/components/coffe/notificationReviews.js
+++ src/components/coffe/notificationReviews.js
@@ -1,7 +1,14 @@
-import { useState } from 'react';
+import PropTypes from 'prop-types';
 
-export default function Notification() {
-  const [message] = useState('');
+const Notification = ({ message }) => {
+  Notification.defaultProps = {
+    message: '',
+  };
+  Notification.propTypes = {
+    message: PropTypes.string,
+  };
 
   return <p>{message}</p>;
-}
+};
+
+export default Notification;
